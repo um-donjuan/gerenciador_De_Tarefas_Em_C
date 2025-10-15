649101ea3873f16828d610e8cd842e32f948460f
Juan Meneses <156260693+um-donjuan@users.noreply.github.com>
2025-10-15 18:26:26 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+/* 
+ Projeto: Gerenciador de Tarefas
+ Autor: Juan Fernandes Reis de Meneses / Leandro Jesus Santos
+Data de criação: [15/10/2025]
+ Descrição: Estrutura inicial do Gerenciador de tarefas.
+*/
+
+#include <stdio.h>
+
+int main () {
+
+    printf("Início do projeto integrador. Gerenciador de Tarefas.\n");
+
+    return 0;
+}
